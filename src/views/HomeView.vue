e2b7c5e66e293c50d760d6d94bc075b2e3fbbc06
briansomogyi <brian.somogyi@emanuel.ro>
2025-04-03 12:12:02 +0300

    Added the GET, POST, and DELETE HTTP methods

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,4 @@
 </template>
 
 <script>
-var xhttp = new XMLHttpRequest()
-xhttp.open("GET", "http://localhost:3000/", true)
-xhttp.send()
-xhttp.onreadystatechange = function () {
-    console.log(this.readyState)
-    console.log(this.status)
-    console.log(this.responseText)
-}
 </script>
